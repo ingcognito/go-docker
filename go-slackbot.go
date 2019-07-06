7f25791db8f3834fd8cab41e5e80d17498940e95
noah-ing <ing.noah@gmail.com>
2019-07-06 08:04:15 -0400

    Create basic server with simple response

--- go-slackbot.go
+++ go-slackbot.go
@@ -1,20 +1,78 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"gopkg.in/resty.v1"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Intersect"
+	}
+	log.Printf("Received request for %s\n", name)
+	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+}
+
 func main() {
+	// Create Server and Route Handlers
+	r := mux.NewRouter()
 
-// GET request
-resp, err := resty.R().Get("http://httpbin.org/get")
+	r.HandleFunc("/", handler)
 
-// explore response object
-fmt.Printf("\nError: %v", err)
-fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
-fmt.Printf("\nResponse Status: %v", resp.Status())
-fmt.Printf("\nResponse Time: %v", resp.Time())
-fmt.Printf("\nResponse Received At: %v", resp.ReceivedAt())
-fmt.Printf("\nResponse Body: %v", resp)     // or resp.String() or string(resp.Body())
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Configure Logging
+	LOG_FILE_LOCATION := os.Getenv("LOG_FILE_LOCATION")
+	if LOG_FILE_LOCATION != "" {
+		log.SetOutput(&lumberjack.Logger{
+			Filename:   LOG_FILE_LOCATION,
+			MaxSize:    50, // megabytes
+			MaxBackups: 3,
+			MaxAge:     28,   //days
+			Compress:   true, // disabled by default
+		})
+	}
+
+	// Start Server
+	go func() {
+		log.Println("Starting Server, it's alive!")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Graceful Shutdown
+	waitForShutdown(srv)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down, goodbye")
+	os.Exit(0)
 }
